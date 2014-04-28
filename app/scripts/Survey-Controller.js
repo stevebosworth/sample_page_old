2b84835f97df493eea0a72fedc071ee226e90106
stevebosworth <stevebosworth@gmail.com>
2014-04-27 20:13:58 -0400

    some attempts at creating OOP js

---
+++ app/scripts/Survey-Controller.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var $ = require('jquery');
+var _ = require('lodash');
+
+  /**
+    *   AdditionalProductInfo is a module.
+    *   The constructor sets up jQuery elements and event listeners, then
+    *   fires the component ready event.
+    *
+    *   Please include any special notes about the module and its constructor here, or
+    *   remove these two lines.
+    *
+    *   @class AdditionalProductInfo
+    *   @extends EGroceryModule
+    *   @constructor
+    */
+var SurveyController = function() {
+    this.initVariables();
+    this.init();
+    this.initEvents();
+
+    this.isInit = true;
+};
+
+SurveyController.prototype = _.create({
+    constructor: SurveyController,
+
+    /**
+    *   Initialize any variables needed, meaning mainly "wrap elements with jQuery."
+    *
+    *   @method initVariables
+    *   @return {undefined}
+    */
+    initVariables: function() {
+
+    },
+
+    /**
+    *   Initialize this module
+    *
+    *   @method init
+    *   @return {undefined}
+    */
+    init: function() {
+        console.log('IT WORKS BITCH');
+    },
+
+    /**
+    *   Initialize events and event listeners.
+    *
+    *   @method initEvents
+    *   @return {undefined}
+    */
+    initEvents: function() {
+
+    },
+});
+
+module.exports = function(){
+    return  SurveyController;
+};
