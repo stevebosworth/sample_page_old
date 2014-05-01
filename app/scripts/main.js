f63f60f25274962a0a8062b09ae1d456b0418c14
stevebosworth <stevebosworth@gmail.com>
2014-05-01 01:03:34 -0400

    updated scss to use media queries, resolved block-grid issue, simplified js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,63 +1,9 @@
 'use strict';
 
 var $ = require('jquery');
-var _ = require('lodash');
 
-var SurveyController = require('./modules/survey.js');
+var $inputs = $('label');
 
-/**
- *
- *  Global class for handling sitewide JS like initiating breakpoint specific JS
- *
- *  @class Global
- *  @constructor
-*/
-var Global = function() {
+$inputs.on('click',function(){
 
-    this.init();
-
-};
-
-Global.prototype = _.create({
-    constructor: Global,
-
-    /**
-     *  Class Init Function.
-     *
-     *  @method init
-     *
-     *  @return {undefined}
-     */
-
-    init: function() {
-
-        this.initVariables();
-        this.initEvents();
-
-        this.initSurvey();
-    },
-
-    initVariables: function() {
-        // Initialize your page specific jQuery selectors here.
-    },
-
-    initSurvey:  function(){
-        SurveyController();
-    },
-
-    /**
-    *   Initialize page specific events and event listeners.
-    *
-    *   @method initEvents
-    *   @return {undefined}
-    */
-    initEvents: function() {
-
-    },
-});
-
-// Pages are built like classes, but they don't return themselves.
-// They instantiate themselves ondomready instead!
-$(function() {
-    var page = new Global();
 });
